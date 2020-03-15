2f9db3d3941acf64084ea606288d29413ec24ded
Gea-Suan Lin <gslin@gslin.org>
2020-03-16 06:16:55 +0800

    Add basic softwares.

--- provision-katago.sh
+++ provision-katago.sh
@@ -8,3 +8,8 @@ echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 
 echo -e "net.core.default_qdisc=fq\nnet.ipv4.tcp_congestion_control=bbr" | sudo tee /etc/sysctl.d/99-tcp.conf
 sudo sysctl -p /etc/sysctl.d/99-tcp.conf
+
+sudo apt update
+sudo env DEBIAN_FRONTEND=noninteractive apt -y upgrade
+sudo apt install -y apache2-utils apt-transport-https build-essential curl dnsutils dstat git jq locales moreutils most mtr-tiny net-tools p7zip-full pigz rsync sharutils software-properties-common sysstat unrar unzip vim-nox wget zsh zsh-syntax-highlighting zstd
+sudo apt clean
