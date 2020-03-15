ecacc263ca0dd4516663706166ea10c55028a61e
Gea-Suan Lin <gslin@gslin.org>
2020-03-16 06:17:35 +0800

    Add katago/lizzie dependencies.

--- provision-katago.sh
+++ provision-katago.sh
@@ -11,5 +11,5 @@ sudo sysctl -p /etc/sysctl.d/99-tcp.conf
 
 sudo apt update
 sudo env DEBIAN_FRONTEND=noninteractive apt -y upgrade
-sudo apt install -y apache2-utils apt-transport-https build-essential curl dnsutils dstat git jq locales moreutils most mtr-tiny net-tools p7zip-full pigz rsync sharutils software-properties-common sysstat unrar unzip vim-nox wget zsh zsh-syntax-highlighting zstd
+sudo apt install -y apache2-utils apt-transport-https build-essential cmake curl default-jdk dnsutils dstat git jq libboost-filesystem-dev libgoogle-perftools-dev libzip-dev locales moreutils most mtr-tiny net-tools nvidia-compute-utils-435 nvidia-driver-435 nvidia-headless-435 nvidia-utils-435 ocl-icd-opencl-dev p7zip-full pigz rsync sharutils software-properties-common sysstat tightvncserver unrar unzip vim-nox wget zsh zsh-syntax-highlighting zstd zlib1g-dev
 sudo apt clean
