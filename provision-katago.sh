3733f72f991862187c845075092c7b40084dab4d
Gea-Suan Lin <gslin@gslin.org>
2020-03-22 16:37:28 +0800

    Expand to one item per line.

--- provision-katago.sh
+++ provision-katago.sh
@@ -11,7 +11,48 @@ sudo sysctl -p /etc/sysctl.d/99-tcp.conf
 
 sudo apt update
 sudo env DEBIAN_FRONTEND=noninteractive apt -y upgrade
-sudo apt install -y apache2-utils apt-transport-https build-essential cmake curl default-jdk dnsutils dstat git jq libboost-filesystem-dev libgoogle-perftools-dev libzip-dev locales moreutils most mtr-tiny net-tools nvidia-compute-utils-435 nvidia-driver-435 nvidia-headless-435 nvidia-utils-435 ocl-icd-opencl-dev p7zip-full pigz rsync sharutils software-properties-common sysstat tightvncserver unrar unzip vim-nox wget zsh zsh-syntax-highlighting zstd zlib1g-dev
+sudo apt install -y \
+     \
+     \
+     \
+    apache2-utils \
+    apt-transport-https \
+    build-essential \
+    cmake \
+    curl \
+    default-jdk \
+    dnsutils \
+    dstat \
+    git \
+    jq \
+    libboost-filesystem-dev \
+    libgoogle-perftools-dev \
+    libzip-dev \
+    locales \
+    moreutils \
+    most \
+    mtr-tiny \
+    net-tools \
+    nvidia-compute-utils-435 \
+    nvidia-driver-435 \
+    nvidia-headless-435 \
+    nvidia-utils-435 \
+    ocl-icd-opencl-dev \
+    p7zip-full \
+    pigz \
+    rsync \
+    sharutils \
+    software-properties-common \
+    sysstat \
+    tightvncserver \
+    unrar \
+    unzip \
+    vim-nox \
+    wget \
+    zsh \
+    zsh-syntax-highlighting \
+    zstd \
+    zlib1g-dev
 sudo apt clean
 
 # Lizzie
